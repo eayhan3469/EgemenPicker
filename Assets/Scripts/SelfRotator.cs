c802422fdea410eaf8860696ba29e7066301ea40
Egemen Ayhan <eayhan3469@gmail.com>
2020-11-28 15:15:17 +0300

    Picker movement.

---
+++ Assets/Scripts/SelfRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotator : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 100;
+
+    void Update()
+    {
+        transform.Rotate(0, 0, Speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SinusoidalMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinusoidalMove : GameManager
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private float frequency = 5f;
+
+    [SerializeField]
+    private float magnitude = 5f;
+
+    private Vector3 position;
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+
+    void Update()
+    {
+        if (DroneHasStart)
+        {
+            position += transform.forward * Time.deltaTime * moveSpeed;
+            transform.position = position + transform.right * Mathf.Sin(Time.time * frequency) * magnitude;
+        }
+    }
+}
