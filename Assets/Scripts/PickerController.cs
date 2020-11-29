67498cb3f2d7a22796bfb58582c0d05a33343e28
Egemen Ayhan <eayhan3469@gmail.com>
2020-11-29 17:18:28 +0300

    Magnet effect added.

--- Assets/Scripts/PickerController.cs
+++ Assets/Scripts/PickerController.cs
@@ -24,14 +24,30 @@ public class PickerController : MonoBehaviour
         {
             transform.Translate(transform.forward * Time.deltaTime * _pickerSpeed);
 
+#if UNITY_EDITOR
             if (Input.GetMouseButton(0))
             {
                 var mouseX = Input.GetAxis("Mouse X");
                 var mouseY = Input.GetAxis("Mouse Y");
 
-                var touch = Vector3.Slerp(transform.position, transform.position + new Vector3(mouseX, 0f, mouseY), TouchSpeed * Time.deltaTime);
+                var touch = Vector3.Slerp(transform.position, transform.position + new Vector3(mouseX, 0f, mouseY), TouchSpeed * Time.deltaTime * 1000);
                 transform.position = new Vector3(Mathf.Clamp(touch.x, -Clamps.x, Clamps.x), transform.position.y, transform.position.z);
             }
+#else
+       if (Input.touchCount > 0)
+            {
+                _touch = Input.GetTouch(0);
+                Debug.Log("asdf");
+
+                if (_touch.phase == TouchPhase.Moved)
+                {
+                    transform.position = new Vector3(
+                        Mathf.Clamp(transform.position.x, -Clamps.x, Clamps.x) + _touch.deltaPosition.x * TouchSpeed,
+                        transform.position.y,
+                        transform.position.z + _touch.deltaPosition.y * TouchSpeed);
+                }
+            }
+#endif
         }
     }
 
@@ -58,7 +74,13 @@ public class PickerController : MonoBehaviour
         if (other.tag == "Object")
         {
             if (GameManager.Instance.LockPicker)
+            {
                 other.GetComponent<Rigidbody>().AddForce(Vector3.forward, ForceMode.Impulse);
+            }
+            else
+            {
+                other.GetComponent<Rigidbody>().AddForce(transform.position - other.GetComponent<Rigidbody>().position * 100f * Time.fixedDeltaTime);
+            }
         }
     }
 }
